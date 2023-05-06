7cbafa1a4d25ad6b4fb21fe8dc28aefc72233ecf
Jamila <jamilaspears.dev@gmail.com>
2023-05-06 15:56:52 -0400

    Cleaned up code and added comments.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,28 +1,13 @@
-// const commentInfo= [
-//      {
-//         name: 'Connor Walton',
-//         time: '02/17/2021',
-//         text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-//      },
-
-//      {
-//         name: 'Emilie Beach',
-//         time: '01/09/2021',
-//         text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-//      },
-
-//      {
-//         name: 'Miles Acosta',
-//         time: '12/20/2020',
-//         text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//      },
-// ]
 
 const commentSection = document.querySelector('.comments__section');
 
+// API URl + Comments + API Key 
 const commentsURL = 'https://project-1-api.herokuapp.com/comments?api_key=fa5afeea-d74a-4045-9408-860afa912b92'
+
+// Initialize userData with open array to use globally 
 let userData = []
 
+// Function to get comments from API and sort in date order from most recent 
 function getComments () {
 axios
    .get(commentsURL)
@@ -38,7 +23,7 @@ axios
    })
 }
 
-
+// Function to create elements, add data from API and display comments on bio page 
 function displayComment(userData) {
     commentSection.innerText = '';
 
@@ -70,11 +55,10 @@ function displayComment(userData) {
 
 
     commentName.innerText = userInfo.name;
-   commentDate.innerText = new Date (userInfo.timestamp).toLocaleDateString()
+    commentDate.innerText = new Date (userInfo.timestamp).toLocaleDateString()
     commentText.innerText = userInfo.comment;
     
-  
-   
+
     commentContainer.appendChild(infoContainer);
     infoContainer.appendChild(miniContainer);
     miniContainer.appendChild(commentName);
@@ -86,7 +70,7 @@ function displayComment(userData) {
 }
 
 
-
+// Grabbing comments from form and posting them on site
 const form = document.querySelector('.form__container-input');
 
 
@@ -95,11 +79,6 @@ form.addEventListener('submit', function (event) {
 
   const userNameInput = document.getElementById('userName');
   const userCommentInput = document.getElementById('userComment');
-
-//   const newCommentInfo = {name: userNameInput.value, text: userCommentInput.value}
-//   userData.unshift(newCommentInfo);
-
-//   console.log(newCommentInfo);
   
   axios.post(commentsURL, {
    name: userNameInput.value,
@@ -107,18 +86,13 @@ form.addEventListener('submit', function (event) {
   })
   .then((response) => {
       console.log (response)
-      // userData.unshift(response.data)
       getComments()
-      //  displayComment()
-    
     })
   
-    
-// displayComment(userData)
 
   userCommentInput.value = '';
   userNameInput.value = '';
  
 });
-// displayComment (userData)
-getComments ()
+
+getComments ();
